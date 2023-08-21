84eb4b1845de468f70cd38d7fe062e75ae3a33fc
edwin12-2 <134753653+edwin12-2@users.noreply.github.com>
2023-08-21 02:28:55 -0500

    Add files via upload

---
+++ web12/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web12.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Biblioteca de libros.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Página de contacto.";
+
+            return View();
+        }
+    }
+}
---
+++ web12/Controllers/LibroController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web12.Models;
+
+namespace web12.Controllers
+{
+    public class LibroController : Controller
+    {
+        // GET: Libro
+        public ActionResult Index()
+        {
+            using (DBModels cModel = new DBModels())
+            {
+                return View(cModel.libros.ToList());
+            }
+        }
+
+        // GET: Libro/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DBModels cModel = new DBModels())
+            {
+                return View(cModel.libros.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Libro/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Libro/Create
+        [HttpPost]
+        public ActionResult Create(libros libro)
+        {
+            try
+            {
+                using (DBModels cModel = new DBModels()) {
+                    cModel.libros.Add(libro);
+                    cModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Libro/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DBModels cModel = new DBModels())
+            {
+                return View(cModel.libros.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Libro/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, libros libro)
+        {
+            try
+            {
+                using (DBModels cModel = new DBModels()) {
+                    cModel.Entry(libro).State = System.Data.EntityState.Modified;
+                    cModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Libro/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DBModels cModel = new DBModels())
+            {
+                return View(cModel.libros.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Libro/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (DBModels cModel = new DBModels()) {
+                    libros libro = cModel.libros.Where(x => x.id == id).FirstOrDefault();
+                    cModel.libros.Remove(libro);
+                    cModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
